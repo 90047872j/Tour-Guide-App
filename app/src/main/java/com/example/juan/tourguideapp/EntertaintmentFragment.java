1e33c765909df6b84397222a639f966ff6bb9eba
Juan <mangasverdes.8@gmail.com>
2017-07-11 22:45:12 +0200

    Rearranged code

--- app/src/main/java/com/example/juan/tourguideapp/EntertaintmentFragment.java
+++ app/src/main/java/com/example/juan/tourguideapp/EntertaintmentFragment.java
@@ -22,10 +22,10 @@ public class EntertaintmentFragment extends Fragment {
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.location_list, container, false);
         final ArrayList<Location> locations = new ArrayList<>();
-        locations.add(new Location(getString(R.string.casinoName),getString(R.string.casinoWebPage),getString(R.string.casinoAddress),getString(R.string.casinoTelephone),getString(R.string.casinoEmail),R.drawable.casino_barcelona));
-        locations.add(new Location(getString(R.string.bodegaName),getString(R.string.bodegaWebPage),getString(R.string.bodegaAddress),getString(R.string.bodegaTelephone),getString(R.string.bodegaEmail),R.drawable.bodega_flamenca));
-        locations.add(new Location(getString(R.string.tarantosName),getString(R.string.tarantoseWebPage),getString(R.string.tarantosAddress),getString(R.string.tarantosTelephone),getString(R.string.tarantosEmail),R.drawable.tarantos));
-        locations.add(new Location(getString(R.string.jamboreeName),getString(R.string.jamboreeWebPage),getString(R.string.jamboreeAddress),getString(R.string.jamboreeTelephone),getString(R.string.jamboreeEmail),R.drawable.jamboree));
+        locations.add(new Location(getString(R.string.casinoName), getString(R.string.casinoWebPage), getString(R.string.casinoAddress), getString(R.string.casinoTelephone), getString(R.string.casinoEmail), R.drawable.casino_barcelona));
+        locations.add(new Location(getString(R.string.bodegaName), getString(R.string.bodegaWebPage), getString(R.string.bodegaAddress), getString(R.string.bodegaTelephone), getString(R.string.bodegaEmail), R.drawable.bodega_flamenca));
+        locations.add(new Location(getString(R.string.tarantosName), getString(R.string.tarantoseWebPage), getString(R.string.tarantosAddress), getString(R.string.tarantosTelephone), getString(R.string.tarantosEmail), R.drawable.tarantos));
+        locations.add(new Location(getString(R.string.jamboreeName), getString(R.string.jamboreeWebPage), getString(R.string.jamboreeAddress), getString(R.string.jamboreeTelephone), getString(R.string.jamboreeEmail), R.drawable.jamboree));
         LocationAdapter itemsAdapter = new LocationAdapter(getActivity(), locations);
         final ListView listView = (ListView) rootView.findViewById(R.id.list);
         listView.setAdapter(itemsAdapter);
--- app/src/main/java/com/example/juan/tourguideapp/MuseumFragment.java
+++ app/src/main/java/com/example/juan/tourguideapp/MuseumFragment.java
@@ -8,7 +8,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
 
-
 import java.util.ArrayList;
 
 public class MuseumFragment extends Fragment {
@@ -23,10 +22,10 @@ public class MuseumFragment extends Fragment {
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.location_list, container, false);
         final ArrayList<Location> locations = new ArrayList<>();
-        locations.add(new Location(getString(R.string.mCampNouName),getString(R.string.mCampNouWebPage),getString(R.string.mCampNouAddress),getString(R.string.mCampNouTelephone),getString(R.string.mCampNouEmail),R.drawable.camp_nou));
-        locations.add(new Location(getString(R.string.mOlimpicName),getString(R.string.mOlimpicWebPage),getString(R.string.mOlimpicAddress),getString(R.string.mOlimpicTelephone),getString(R.string.mOlimpicEmail),R.drawable.museu_olimpic));
-        locations.add(new Location(getString(R.string.mHistoriaName),getString(R.string.mHistoriaWebPage),getString(R.string.mHistoriaAddress),getString(R.string.mHistoriaTelephone),getString(R.string.mHistoriaEmail),R.drawable.museu_historia));
-        locations.add(new Location(getString(R.string.mNacionalName),getString(R.string.mNacionalWebPage),getString(R.string.mNacionalAddress),getString(R.string.mNacionalTelephone),getString(R.string.mNacionalEmail),R.drawable.museu_art));
+        locations.add(new Location(getString(R.string.mCampNouName), getString(R.string.mCampNouWebPage), getString(R.string.mCampNouAddress), getString(R.string.mCampNouTelephone), getString(R.string.mCampNouEmail), R.drawable.camp_nou));
+        locations.add(new Location(getString(R.string.mOlimpicName), getString(R.string.mOlimpicWebPage), getString(R.string.mOlimpicAddress), getString(R.string.mOlimpicTelephone), getString(R.string.mOlimpicEmail), R.drawable.museu_olimpic));
+        locations.add(new Location(getString(R.string.mHistoriaName), getString(R.string.mHistoriaWebPage), getString(R.string.mHistoriaAddress), getString(R.string.mHistoriaTelephone), getString(R.string.mHistoriaEmail), R.drawable.museu_historia));
+        locations.add(new Location(getString(R.string.mNacionalName), getString(R.string.mNacionalWebPage), getString(R.string.mNacionalAddress), getString(R.string.mNacionalTelephone), getString(R.string.mNacionalEmail), R.drawable.museu_art));
         LocationAdapter itemsAdapter = new LocationAdapter(getActivity(), locations);
         final ListView listView = (ListView) rootView.findViewById(R.id.list);
         listView.setAdapter(itemsAdapter);
